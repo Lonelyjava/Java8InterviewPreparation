81165feec7f8657980de99a63bbd95c553629246
kunda <kunda@LAPTOP-NJOTD0LD>
2022-03-12 22:49:34 +0530

    commit

--- Java8InterviewPreparation/src/main/java/com/example/demo/controller/DemoEight.java
+++ Java8InterviewPreparation/src/main/java/com/example/demo/controller/DemoEight.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 public class DemoEight {
 
 	public static void main(String[] args) {
 		System.out.println("Stat learning java 1.8 with positive motivation ....by kundan kumar ");
 
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for(int i=0;i<10;i++)
+		{
+			arrayList.add(i);
+		}
+		System.out.println("Arraylist" +arrayList);
 		
+		for(Integer i:arrayList) {
+			System.out.println("Iterate the value from array .."+i);
+		}
 	}
 }
