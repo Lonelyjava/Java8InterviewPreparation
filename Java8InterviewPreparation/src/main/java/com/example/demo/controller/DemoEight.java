91866699ed9339924855d0be79a1321c6f60c7c7
kunda <kunda@LAPTOP-NJOTD0LD>
2022-03-12 20:19:24 +0530

    code commit for preparation of java 1.8 for learning in master java application and also for interview of mnc

---
+++ Java8InterviewPreparation/src/main/java/com/example/demo/controller/DemoEight.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+public class DemoEight {
+
+	public static void main(String[] args) {
+		System.out.println("Stat learning java 1.8 with positive motivation ....by kundan kumar ");
+
+		
+	}
+}
