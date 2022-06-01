0707c28c36533e103f6ec6e93e495e4c2a32ab42
kunda <kunda@LAPTOP-NJOTD0LD>
2022-06-01 20:52:36 +0530

    commit for generate QR code

--- Java8InterviewPreparation/src/main/java/com/example/demo/controller/GenerateQRCode.java
+++ Java8InterviewPreparation/src/main/java/com/example/demo/controller/GenerateQRCode.java
@@ -7,5 +7,8 @@ public class GenerateQRCode {
 	public static void main(String agr[])
 	{
 		System.out.println("kundan here ..");
+		for(int i=0;i<10;i++) {
+			System.out.println("demo" );
+		}
 	}
 }
